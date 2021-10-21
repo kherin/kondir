b6206c85b4cda1f67076c1079f662fd7fbb068fe
Kherin <kherin.j.s.bundhoo@gmail.com>
2021-10-21 17:52:46 +0400

    feature: added quiz screen

--- lib/screens/displayPicture.dart
+++ lib/screens/displayPicture.dart
@@ -1,19 +1,148 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'dart:math';
+import 'package:basic_utils/basic_utils.dart';
 
 // A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
+  final String rightAnswer;
 
-  const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
+  const DisplayPictureScreen({
+    Key key,
+    this.imagePath,
+    this.rightAnswer,
+  }) : super(key: key);
+
+  String transformLabel(String label) {
+    var removedUnderScore = label.replaceAll("_", " ");
+    return StringUtils.capitalize(removedUnderScore);
+  }
+
+  void verifyIfRightAnswer(String value, BuildContext context) {
+    var msg = "Wrong Answer";
+    if (this.transformLabel(this.rightAnswer) == value) {
+      msg = "Right Answer";
+    }
+    final snackBar = SnackBar(
+      content: Text(msg),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 
   @override
   Widget build(BuildContext context) {
+    var randomGenerator = new Random();
+    var selectedOptionLabels = [];
+    const optionLabels = [
+      "20_speed",
+      "30_speed",
+      "50_speed",
+      "60_speed",
+      "70_speed",
+      "80_speed",
+      "80_lifted",
+      "100_speed",
+      "120_speed",
+      "no_overtaking_general",
+      "no_overtaking_trucks",
+      "right_of_way_crossing",
+      "right_of_way_general",
+      "give_way",
+      "stop",
+      "no_way_general",
+      "no_way_trucks",
+      "no_way_one_way",
+      "attention_general",
+      "attention_left_turn",
+      "attention_right_turn",
+      "attention_curvy",
+      "attention_bumpers",
+      "attention_slippery",
+      "attention_bottleneck",
+      "attention_construction",
+      "attention_traffic_light",
+      "attention_pedestrian",
+      "attention_children",
+      "attention_bikes",
+      "attention_snowflake",
+      "attention_deer",
+      "lifted_general",
+      "turn_right",
+      "turn_left",
+      "turn_straight",
+      "turn_straight_right",
+      "turn_straight_left",
+      "turn_right_down",
+      "turn_left_down",
+      "turn_circle",
+      "lifted_no_overtaking_general",
+      "lifted_no_overtaking_trucks"
+    ];
+
+    for (var index = 0; index < 3; index++) {
+      var labelPosition = randomGenerator.nextInt(optionLabels.length - 1);
+      selectedOptionLabels.add(transformLabel(optionLabels[labelPosition]));
+    }
+
+    if (rightAnswer != null) {
+      selectedOptionLabels.add(transformLabel(rightAnswer));
+    } else {
+      print("Right answer is null");
+    }
+    // var parsedOptionLabels = optionLabels.
+    var options = <Widget>[];
+
+    for (var label in selectedOptionLabels) {
+      Widget option = Column(children: <Widget>[
+        SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            SizedBox(
+              width: 10,
+            ), //SizedBox
+            Text(
+              label,
+              style: TextStyle(fontSize: 20),
+            ), //Text
+            SizedBox(width: 10), //SizedBox
+            /** Checkbox Widget **/
+            Checkbox(
+              value: false,
+              onChanged: (value) {
+                verifyIfRightAnswer(label, context);
+              },
+            ), //Checkbox
+          ], //<Widget>[]
+        ), //Row
+      ]);
+
+      options.add(option);
+    }
     return Scaffold(
-      appBar: AppBar(title: const Text('Display the Picture')),
-      // The image is stored as a file on the device. Use the `Image.file`
-      // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
+      appBar: AppBar(
+        title: const Text('Identify the Traffic Sign'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Image.file(
+              File(imagePath),
+              height: 400,
+            ),
+          ),
+          Text(
+            'Which traffic sign is it ?',
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ),
+          Column(
+            children: options,
+          ), //Column
+        ],
+      ),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -11,12 +12,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
